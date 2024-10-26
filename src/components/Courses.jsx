550e9a8ed8e3790dd3c97a8edebbb3417bbfdb4d
yogendra singh <yogendrayadav09@yahoo.com>
2024-10-27 03:33:10 +0530

    url

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -11,7 +11,7 @@ const Courses = () => {
 
   return (
     <div className="w-full py-20 flex items-start justify-center gap-5 flex-wrap mx-auto px-20 max-sm:w-full max-sm:px-5">
-      {allcourse.map((course) => (
+      {allcourse.length > 0 && allcourse?.map((course) => (
         <Coursescard course={course} key={course._id} admin={admin} id={course._id} />
       ))}
     </div>
